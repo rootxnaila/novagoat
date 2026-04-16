bd2e89b5d635be2f51408e3a22c3762e2495dc71
Pallnova07 <nova.indri0706@gmail.com>
2026-04-17 06:38:08 +0700

    feat: slicing dashboard dan katalog fix

---
+++ resources/views/katalog/katalog.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Katalog Kambing</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+    .card-img-top-equal {
+        height: 250px; 
+        object-fit: cover; 
+    }
+</style>
+</head>
+<body class="bg-light">
+    <div class="container mt-5">
+        <h2 class="mb-4">Ensiklopedia Kambing</h2>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://www.etawajaya.com/wp-content/uploads/2022/06/kambing-etawa-indonesia.jpg" class="card-img-top-equal" alt="Foto Kambing Ras Etawa">
+                    <div class="card-body">
+                        <h5 class="card-title">Si Jalu</h5>
+                        <p class="card-text text-muted">Ras: Ettawa</p>
+                        <p class="card-text"><strong>Berat:</strong> 50-75 kg</p>
+                    </div>
+                    <div class="card-footer text-center">
+                        <button class="btn btn-outline-primary btn-sm">Detail</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://trubus.id/wp-content/uploads/2022/09/Enam-Keunggulan-Kambing-Boerka-696x516.jpg" class="card-img-top-equal" alt="Foto Kambing Ras boer">
+                    <div class="card-body">
+                        <h5 class="card-title">Si Putih</h5>
+                        <p class="card-text text-muted">Ras: Boer</p>
+                        <p class="card-text"><strong>Berat:</strong> 50-80 kg</p>
+                    </div>
+                    <div class="card-footer text-center">
+                        <button class="btn btn-outline-primary btn-sm">Detail</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

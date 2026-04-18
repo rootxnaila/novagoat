d63375b69f340e154787155b78cf9007d181845d
nailarizki <hello.nailarizki@gmail.com>
2026-04-18 16:37:09 +0700

    feat : tambah api ping untuk demo git ke fe

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,8 @@ use App\Http\Controllers\KambingController;
 
 Route::get('/kambing', [KambingController::class, 'index']); //GET /api/kambing 
 Route::get('/dashboard/stats', [DashboardController::class, 'stats']); //GET /api/dashboard/stats
+Route::get('/ping', function() {
+    return response()->json(['status' => 'success', 'message' => 'Pong! Server Kambing Pak Tarno siap melayani!',
+    'waktu_Sekarang' => now()
+    ]);
+});
